9b5ceeb08938342dd540c3da7ccc086a3bf698ca
blitzkz23 <blitzaldykazu@gmail.com>
2022-12-13 05:15:00 +0700

    commit: untracked commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # 07-kafka-twitter-streaming
+
+This is a simple example of how to use Kafka to stream tweets from Twitter's API.
+
+## How to run
+
+1. Clone the repository
+```bash
+git clone https://github.com/df8-naufal-aldy-pradana/07-kafka-twitter-streaming.git
+```
+
+2. Change the twitter credential used for auth on src/twitter_credentials.py, you can get it from [here](https://developer.twitter.com/en/apps)
+
+3. You can also change the twitter_stream.py func to fit your needs better, you can find the documentation [here](https://developer.twitter.com/en/docs/tweets/filter-realtime/api-reference/post-statuses-filter)
+
+4. Run docker-compose
+```bash
+docker-compose up
+```
--- kafka_producer.py
+++ kafka_producer.py
@@ -1,12 +1,9 @@
 from confluent_kafka import Producer
-from faker import Faker
 import json
 import time
 import logging
-import random
-from twitter_etl import run_twitter_etl
-
-fake=Faker()
+from src.twitter_stream import run_twitter_stream
+import src.twitter_credentials as token
 
 logging.basicConfig(format='%(asctime)s %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
@@ -17,32 +14,41 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 ####################
-p=Producer({'bootstrap.servers':'localhost:9092'})
+p = Producer({'bootstrap.servers': 'localhost:9092'})
 print('Kafka Producer has been initiated...')
 #####################
-def receipt(err,msg):
+
+
+def receipt(err, msg):
     if err is not None:
         print('Error: {}'.format(err))
     else:
-        message = 'Produced message on topic {} with value of {}\n'.format(msg.topic(), msg.value().decode('utf-8'))
+        message = 'Produced message on topic {} with value of {}\n'.format(
+            msg.topic(), msg.value().decode('utf-8'))
         logger.info(message)
         print(message)
-        
+
 #####################
+
+
 def main():
-    for i in range(50):
-        data={
-           'user_id': fake.random_int(min=20000, max=100000),
-           'user_name':fake.name(),
-           'user_address':fake.street_address() + ' | ' + fake.city() + ' | ' + fake.country_code(),
-           'platform': random.choice(['Mobile', 'Laptop', 'Tablet']),
-           'signup_at': str(fake.date_time_this_month())    
-           }
-        m=json.dumps(data)
+    for tweet in tweets:
+        text = tweet._json["full_text"]
+
+        refined_tweet = {
+            "user": tweet.user.screen_name,
+            "text": text,
+            "favorite_count": tweet.favorite_count,
+            "retweet_count": tweet.retweet_count,
+            "created_at": tweet.created_at,
+        }
+        m = json.dumps(refined_tweet, indent=4, sort_keys=True, default=str)
         p.poll(1)
-        p.produce('user-tracker', m.encode('utf-8'),callback=receipt)
+        p.produce('twitter-tracker', m.encode('utf-8'), callback=receipt)
         p.flush()
-        time.sleep(3)
-        
+        time.sleep(1)
+
+
 if __name__ == '__main__':
+    tweets = run_twitter_stream(token)
     main()
