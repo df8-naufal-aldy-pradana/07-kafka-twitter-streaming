e59d4db19ea4361bf2d465f333e192883b26f141
blitzkz23 <blitzaldykazu@gmail.com>
2022-12-13 04:10:29 +0700

    commit local changes

--- twitter_etl.py
+++ twitter_etl.py
@@ -6,7 +6,6 @@ from datetime import datetime
 def run_twitter_etl():
     
     # Twitter API credentials
-    bearer_token = "AAAAAAAAAAAAAAAAAAAAAAf1jwEAAAAAQBido%2BDdSaG63Juo%2Ba0%2FxhV%2BHLE%3D1iiyYrgEWNdrufvoZ6JujZnVtGSk3PNWQvuXvD3Da5qEOIHHMA"
     access_key = "U8dxRixLB4hnsmK6ETUIuyj6o"
     access_secret = "q0wCMs8CeK2QDFbTgwHPjSSGYrkfYmQm2glJqK4c0aMZejIMgc"
     consumer_key = "432387030-IFGBvr1ICNDPJLcKd9JUnOlnjtXErwTASOcIFuJI"
@@ -30,25 +29,23 @@ def run_twitter_etl():
                             # otherwise only the first 140 words are extracted
                             tweet_mode = 'extended'
                             )
+    
+    return tweets
 
-    # Create a dataframe from the tweets
-    tweet_list = []
-    for tweet in tweets:
-        text = tweet._json["full_text"]
+    # # Create a dataframe from the tweets
+    # tweet_list = []
+    # for tweet in tweets:
+    #     text = tweet._json["full_text"]
 
-        refined_tweet = {"user": tweet.user.screen_name,
-                        "text": text,
-                        "favorite_count": tweet.favorite_count,
-                        "retweet_count": tweet.retweet_count,
-                        "created_at": tweet.created_at,}
-        # with open("tweet.json", "w") as f:
-        #     json.dump(refined_tweet, f)
-        tweet_list.append(refined_tweet)
+    #     refined_tweet = {"user": tweet.user.screen_name,
+    #                     "text": text,
+    #                     "favorite_count": tweet.favorite_count,
+    #                     "retweet_count": tweet.retweet_count,
+    #                     "created_at": tweet.created_at,}
+    #     # with open("tweet.json", "w") as f:
+    #     #     json.dump(refined_tweet, f)
+    #     tweet_list.append(refined_tweet)
         
-    # Create a dataframe from the tweets, and save it as a csv file    
-    df = pd.DataFrame(tweet_list)
+    # # Create a dataframe from the tweets, and save it as a csv file    
+    # df = pd.DataFrame(tweet_list)
     
-    # TODO: Save the dataframe to S3/GCS
-    df.to_csv("mumei_twitter_data.csv")
-    
-run_twitter_etl()
